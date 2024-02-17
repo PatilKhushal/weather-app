ba21f29c6829e1b4e7d87188f5cc7a36ea1013b1
PatilKhushal <patilkhushal2803@gmail.com>
2024-02-17 19:11:32 +0530

    fetched over https

--- src/fetchData.js
+++ src/fetchData.js
@@ -2,7 +2,7 @@ const key = "316eb9bf8b0e45b385d111956241601";
 export async function getData(city) {
 
   let response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=10&aqi=no&alerts=no`
+    `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=10&aqi=no&alerts=no`
   );
   let result = await response.json();
 
